df19d254bf8ca2517c2bcb4fdb76d710c2700c1a
Bima Febriansyah <bimafebriansyah1002@gmail.com>
2020-01-15 18:04:16 +0700

    cart fix

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const Router = express.Router();
 
 const product = require('./product');
+const cart = require('./cart');
 
 Router.use('/product', product);
+Router.use('/cart', cart);
 
 module.exports = Router;
